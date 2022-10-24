1e85ffc8a700ff582bad38f8b4b162d4628edaa1
adamdgit <adam_d@outlook.com>
2022-10-25 03:31:20 +1100

    Complete re-write, to infinite loop carousel

--- app.js
+++ app.js
@@ -1,147 +1,97 @@
-let CATEGORY_ITEMS = 20
-let SECTION_SIZE = 3
-let QUERY = ''
-let SCREEN_SIZE = document.body.clientWidth
+const body = document.querySelector('body')
+const leftButton = document.querySelector('.slide-left')
+const rightButton = document.querySelector('.slide-right')
+const slider = document.querySelector('.slider')
+let slideIndex = 1 // decrease when clicking right, increase clicking left
+let carouselItems = slider.children.length
+let sectionSize = 3
 
-// set section size based on screen size
-if (SCREEN_SIZE >= 600) {
-  SECTION_SIZE = 3
+// set default items on load
+if (window.innerWidth < 800) {
+  body.style.setProperty('--section-size', '2')
+  sectionSize = 2
 }
-if (SCREEN_SIZE >= 1400) {
-  SECTION_SIZE = 4
+if (window.innerWidth < 500) {
+  body.style.setProperty('--section-size', '1')
+  sectionSize = 1
 }
-if (SCREEN_SIZE >= 2000) {
-  SECTION_SIZE = 5
-} 
-if (SCREEN_SIZE < 600) {
-  SECTION_SIZE = 2
-}
-
-const slide1 = document.querySelector('[data-slide-cat="sci-fi"]')
-const slide2 = document.querySelector('[data-slide-cat="action"]')
-const slide3 = document.querySelector('[data-slide-cat="documentary"]')
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '769cd24bbamsh1fb4150e925e97ap125ffcjsn7c416d1919c5',
-		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-	}
+if (window.innerWidth >= 800) {
+  body.style.setProperty('--section-size', '3')
+  sectionSize = 3
 }
 
-let documentaryResults = []
-let actionResults = []
-let scifiResults = []
-
-// sci-fi
-const getCat1 = async () => {
-  await fetch('https://imdb8.p.rapidapi.com/title/v2/find?title=alien&titleType=movie&limit=20&sortArg=moviemeter%2Casc&genre=sci-fi', options)
-	.then(response => response.json())
-	.then(response => {
-    scifiResults = response.results
-    console.log(response.results)
-    })
-	.catch(err => console.error(err));
-
-  scifiResults.map(result => {
-    slide1.insertAdjacentHTML('beforeend', `
-      <span class="result">
-        <h2>${result.title? result.title : 'no data'}</h2>
-        <img src=${result.image? result.image.url : 'no image data'} alt='${result.title} movie cover' />
-        <h3>${result.year? result.year : 'no data'}</h3>
-      </span>
-    `)
-  })
-}
-getCat1()
-
-// action
-const getCat2 = async () => {
-  await fetch('https://imdb8.p.rapidapi.com/title/v2/find?title=game&titleType=movie&limit=20&sortArg=moviemeter%2Casc&genre=action', options)
-	.then(response => response.json())
-	.then(response => {
-    actionResults = response.results
-    console.log(response.results)
-    })
-	.catch(err => console.error(err));
-
-  actionResults.map(result => {
-    slide2.insertAdjacentHTML('beforeend', `
-      <span class="result">
-        <h2>${result.title}</h2>
-        <img src=${result.image? result.image.url : 'no image data'} alt="" />
-        <h3>${result.year}</h3>
-      </span>
-    `)
-  })
-}
-getCat2()
-
-// documentary
-const getCat3 = async () => {
-  await fetch('https://imdb8.p.rapidapi.com/title/v2/find?title=david&titleType=movie&limit=20&sortArg=moviemeter%2Casc&genre=documentary', options)
-	.then(response => response.json())
-	.then(response => {
-    documentaryResults = response.results
-    console.log(response.results)
-    })
-	.catch(err => console.error(err));
-
-  documentaryResults.map(result => {
-    slide3.insertAdjacentHTML('beforeend', `
-      <span class="result">
-        <h2>${result.title}</h2>
-        <img src=${result.image? result.image.url : 'no image data'} alt="" />
-        <h3>${result.year}</h3>
-      </span>
-    `)
-  })
-}
-getCat3()
-
-// update number of cards in the row when screen size is changed
+// change number of items when screen changes size
 window.addEventListener('resize', () => {
-  SCREEN_SIZE = document.body.clientWidth
-  if (SCREEN_SIZE >= 600) {
-    SECTION_SIZE = 3
+  if (window.innerWidth < 800) {
+    body.style.setProperty('--section-size', '2')
+    sectionSize = 2
   }
-  if (SCREEN_SIZE >= 1400) {
-    SECTION_SIZE = 4
+  if (window.innerWidth < 500) {
+    body.style.setProperty('--section-size', '1')
+    sectionSize = 1
   }
-  if (SCREEN_SIZE >= 2000) {
-    SECTION_SIZE = 5
-  } 
-  if (SCREEN_SIZE < 600) {
-    SECTION_SIZE = 2
-  }
-})
-
-// check if button is clicked, pass clicked button node to handleclick
-document.addEventListener('click', (e) => {
-  let slideBtn
-  if (e.target.matches('.slide-btn')) {
-    slideBtn = e.target
-  }
-  if (slideBtn != null) {
-    handleClick(slideBtn)
+  if (window.innerWidth >= 800) {
+    body.style.setProperty('--section-size', '3')
+    sectionSize = 3
   }
 })
 
-// scroll left or right depending on button clicked
-function handleClick(node) {
+// first and last section of images of the slider
+let rightImage = slider.children[slider.children.length -1]
+let leftImage2 = slider.children[1]
+let leftImage = slider.children[0]
 
-  let slide = node.closest('.video-slider-wrap').querySelector('.slider')
-  let slideIndex = +(slide.style.getPropertyValue('--slider-index'))
-  let slideSections = CATEGORY_ITEMS / SECTION_SIZE
-  // prevent scrolling left if no items to scroll through
-  if (node.classList.contains('slide-left') && slideIndex >= 1) {
-    slide.style.setProperty('--slider-index', slideIndex -1)
+// place clones at start and end of carousel
+let clone = rightImage.cloneNode()
+slider.insertAdjacentElement('afterbegin', clone)
+clone = leftImage.cloneNode()
+slider.insertAdjacentElement('beforeend', clone)
+clone = leftImage2.cloneNode()
+slider.insertAdjacentElement('beforeend', clone)
+
+
+leftButton.addEventListener('pointerdown', () => {
+  console.log(slideIndex)
+  if(slideIndex === 1) {
+    slideIndex--
+    slider.style.transform = `translateX(${Math.round(slideIndex * -(100 / sectionSize))}%)`
+    const timer1 = setTimeout(() => {
+      slider.style.transition = 'unset'
+      slideIndex = carouselItems
+      slider.style.transform = `translateX(${Math.round(slideIndex * -(100 / sectionSize))}%)`
+      clearTimeout(timer1)
+    }, 310)
+    const timer2 = setTimeout(() => {
+      slider.style.transition = 'transform 300ms ease'
+      clearTimeout(timer2)
+    }, 320)
+    return
   }
-  // at end of items return to start of list
-  if (node.classList.contains('slide-right')) {
-    if (slideIndex >= slideSections -1) {
-      slide.style.setProperty('--slider-index', slideIndex = 0)
-    } else {
-      slide.style.setProperty('--slider-index', slideIndex +1)
-    }
+
+  slideIndex--
+  slider.style.transform = `translateX(${Math.round(slideIndex * -(100 / sectionSize))}%)`
+
+})
+
+
+
+rightButton.addEventListener('pointerdown', () => {
+
+  slideIndex++
+  if(slideIndex === carouselItems) {
+    slider.style.transform = `translateX(${Math.round(slideIndex * -(100 / sectionSize))}%)`
+    const timer1 = setTimeout(() => {
+      slider.style.transition = 'unset'
+      slideIndex = 0
+      slider.style.transform = `translateX(${Math.round(slideIndex * -(100 / sectionSize))}%)`
+      clearTimeout(timer1)
+    }, 270)
+    const timer2 = setTimeout(() => {
+      slider.style.transition = 'transform 300ms ease'
+      clearTimeout(timer2)
+    }, 290)
+    return
   }
-}
+  slider.style.transform = `translateX(${Math.round(slideIndex * -(100 / sectionSize))}%)`
+
+})
